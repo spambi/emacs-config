c1f93b89460c04af178e82420f2c384235664104
spambi <spambayyyy@gmail.com>
2021-05-26 21:46:58 +1000

    updates

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (require 'package)
-;(require 'use-package)
-;(require 'evil-collection)
+(require 'use-package)
+(require 'evil-collection)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (package-initialize)
@@ -17,11 +17,53 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3c3836" "#fb4933" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
+ '(auth-source-save-behavior nil)
+ '(custom-safe-themes
+   '("76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "16ab866312f1bd47d1304b303145f339eac46bbc8d655c9bfa423b957aa23cc9" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" "b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" default))
+ '(fci-rule-color "#7c6f64")
  '(helm-minibuffer-history-key "M-p")
- '(org-agenda-files '("~/Dropbox/org/TODO.org")))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#fabd2f"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#b8bb26"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#928374"))
+ '(notmuch-search-line-faces
+   '(("unread" :foreground "#aeee00")
+     ("flagged" :foreground "#0a9dff")
+     ("deleted" :foreground "#ff2c4b" :bold t)))
+ '(objed-cursor-color "#fb4934")
+ '(org-agenda-files '("~/Dropbox/org/capture.org" "~/Dropbox/org/TODO.org"))
+ '(package-selected-packages
+   '(ytdious org-mime mu4e ytel visual-fill-column lexic dictionary org-superstar undo-tree doom-themes lua-mode yasnippet-snippets workgroups2 which-key use-package try sauron rainbow-mode ox-reveal org-super-agenda org-roam-server org-ref org-noter org-journal org-dropbox org-download org-cliplink nyan-mode nov literate-calc-mode ledger-mode ivy-yasnippet iedit gruvbox-theme flyspell-correct-ivy flycheck evil-org evil-magit evil-collection emojify elpy elfeed-org elfeed-goodies doom-modeline dired-subtree dired-rainbow dired-open deft ddskk dashboard counsel company-org-roam company-ledger company-irony-c-headers company-irony company-emoji company-c-headers company-bibtex company-auctex bug-hunter badwolf-theme alert ace-window))
+ '(pdf-view-midnight-colors '("#fdf4c1" . "#1d2021"))
+ '(rustic-ansi-faces
+   ["#282828" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#cc241d" "#8ec07c" "#ebdbb2"])
+ '(vc-annotate-background "#282828")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#b8bb26")
+    (cons 40 "#cebb29")
+    (cons 60 "#e3bc2c")
+    (cons 80 "#fabd2f")
+    (cons 100 "#fba827")
+    (cons 120 "#fc9420")
+    (cons 140 "#fe8019")
+    (cons 160 "#ed611a")
+    (cons 180 "#dc421b")
+    (cons 200 "#cc241d")
+    (cons 220 "#db3024")
+    (cons 240 "#eb3c2c")
+    (cons 260 "#fb4934")
+    (cons 280 "#e05744")
+    (cons 300 "#c66554")
+    (cons 320 "#ac7464")
+    (cons 340 "#7c6f64")
+    (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.5)))))
+(put 'upcase-region 'disabled nil)
